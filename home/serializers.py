3135ef376e666300e0abeafeaea2c4492104ddc9
Raja Kumar <141621819+Raja-kumar7479@users.noreply.github.com>
2025-11-08 00:06:07 +0530

    58451

---
+++ home/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import MenuCategory
+class MenuCategorySerializer(serializers.ModelSerializers):
+    class Meta:
+        model = MenuCategory
+        fields = ['id','name']
