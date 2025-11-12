b3d97626d5a7f68ac80b248d66c96e46eb71bb1e
Raja Kumar <141621819+Raja-kumar7479@users.noreply.github.com>
2025-11-12 22:53:29 +0530

    58447

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import *
+from .views import (
+    MenuCategoryListCreateAPIView
+    MenuCategoryRetrieveUpdateDestroyAPIView
+)
 
 urlpatterns = [
-    
+    path("categories/",MenuCategoryListCreateAPIView.as_view(), name="category-lsit-create"),
+    path("categories/<int:pk>/",MenuCategoryRetrieveUpdateDestroyAPIView.as_view(),name = "category-detail"),
 ]
