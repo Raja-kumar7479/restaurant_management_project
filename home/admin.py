efea5f25bdb9e6101832bfd6feaf16c93e97f429
Raja Kumar <141621819+Raja-kumar7479@users.noreply.github.com>
2025-11-12 22:44:33 +0530

    58447

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-cla
+from home.migrations.models import MenuCategory
+@admin.register(MenuCategory)
+class MenuCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    
