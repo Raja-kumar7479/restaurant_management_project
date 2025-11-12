df303a16e0d3a969c3f7f80f19536f39c54b3802
Raja Kumar <141621819+Raja-kumar7479@users.noreply.github.com>
2025-11-12 22:29:50 +0530

    58447

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100. unique =True)
+    class Meta:
+        verbose_name ="Menu Category"
+        verbose_name_plural ="Menu categories"
+        odering =["name"]
+    def __str__(self):
+        return self.name
