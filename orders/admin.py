a27aa97d501b2aa4d7ab36b70a37a2457add9736
Raja Kumar <141621819+Raja-kumar7479@users.noreply.github.com>
2026-02-01 20:07:35 +0530

    58450

--- orders/admin.py
+++ orders/admin.py
@@ -1,35 +1,5 @@
+from django.contrib import admin
 
-from .models import Coupon
-
-
-@admin.register(Coupon)
-class CouponAdmin(admin.ModelAdmin):
-    list_display = (
-        'code',
-        'discount_percentage',
-        'is_active',
-        'valid_from',
-        'valid_until',
-    )
-
-    list_filter = (
-        'is_active',
-        'valid_from',
-        'valid_until',
-    )
-
-    search_fields = ('code',)
-
-    ordering = ('-valid_from',)
-
-    readonly_fields = ()
-
-    fieldsets = (
-        ('Coupon Details', {
-            'fields': ('code', 'discount_percentage')
-        }),
-        ('Validity', {
-            'fields': ('valid_from', 'valid_until', 'is_active')
-        }),
-    )
-
+# Register your models here.
+from .models import coupone
+@admin couponeAdmin(admin.modelADmin):
